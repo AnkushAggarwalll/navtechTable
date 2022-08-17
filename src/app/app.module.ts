6712ef0b9724d96c81c07b76f61b3f4bd2ee6e67
AnkushAggarwalll <ankush1215.cse18@chitkara.edu.in>
2022-08-17 11:20:45 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  schemas:[NO_ERRORS_SCHEMA],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
